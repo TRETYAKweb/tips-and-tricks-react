927b16ace5321a4bc7e071576586adbb145900e1
TRETYAKweb <tretyakbro@gmail.com>
2023-01-09 06:28:13 +0300

    feat: adding components: Hero, Nav, TipList

---
+++ src/pages/all-pages/index.js
@@ -0,0 +1,11 @@
+import { Hero, Nav, TipList } from '../../components';
+
+export const AllTopicsPage = () => {
+    return (
+        <section className = 'layout'>
+            <Nav />
+            <Hero />
+            <TipList />
+        </section>
+    );
+};
