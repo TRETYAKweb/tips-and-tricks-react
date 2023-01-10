846dd685a67a7a7d3994992b3f8627f840690a39
TRETYAKweb <tretyakbro@gmail.com>
2023-01-10 22:45:33 +0300

    feat(components): implement handleClick for tags

--- src/components/Tags/index.js
+++ src/components/Tags/index.js
@@ -1,20 +1,30 @@
+// Core
+import { useContext } from 'react';
+
+// Helpers
 import { getTagIcon } from '../../helpers';
+import { Context } from '../../lib/selectedTagContext';
 
 import iconsJson from '../../mock-data/tags.json';
 
 export const Tags = () => {
+    const [selectedTagId, setSelectedTagId] = useContext(Context);
+
     const tagsJSX = iconsJson.map(({ id, name }) => {
         const TagIcon = getTagIcon(name);
+        const handleClick = () => setSelectedTagId(id);
 
         return (
             <span
-                data-active = { true } key = { id }
+                onClick = { handleClick }
+                data-active = { selectedTagId === id } key = { id }
                 className = 'tag'>
                 <TagIcon />
                 { name }
             </span>
         );
     });
+
 
     return (
         <div className = 'tags'>
