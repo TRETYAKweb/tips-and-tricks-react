34420b802283be3f6c59a8474fae51819884ea73
TRETYAKweb <tretyakbro@gmail.com>
2023-01-10 22:43:14 +0300

    refactor(components): :recycle: TipList

--- src/components/TipList/Composer/index.js
+++ src/components/TipList/Composer/index.js
@@ -1,9 +1,16 @@
+// Components
 import { Tip } from '../Tip';
 
+// Mock
+
+import tips from '../../../mock-data/tips.json';
+
 export const TipList = () => {
+    const tipsJSX = tips.map((tip) => <Tip key = { tip.id } { ...tip } />);
+
     return (
         <section className = 'tip-list'>
-            <Tip />
+            { tipsJSX }
         </section>
     );
 };
