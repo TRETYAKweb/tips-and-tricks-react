90cb315faa8dda95b8bb6f082be9f0c811dcdc1f
TRETYAKweb <tretyakbro@gmail.com>
2023-01-10 22:50:09 +0300

    feat(components): implement handleClickBtnSettings for settings

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,12 +1,26 @@
+// Core
+import { useContext } from 'react';
+import cx from 'classnames';
+
+// Helpers
+import { Context } from '../../lib/settingsContext';
 
 export const Settings = () => {
+    const [isSettingsOpen, setisSettingsOpen] = useContext(Context);
+    const handleClickBtnSettings = () => setisSettingsOpen(false);
+
+    const settingClasses = cx('settings', {
+        open:   isSettingsOpen,
+        closed: !isSettingsOpen,
+    });
+
     return (
-        <section>
+        <section className = { settingClasses }>
             <header>
                 <h1>Настройки</h1>
             </header>
             <footer>
-                <button>Закрыть</button>
+                <button onClick = { handleClickBtnSettings }>Закрыть</button>
             </footer>
         </section>
     );
