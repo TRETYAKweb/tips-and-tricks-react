fa588bd9f65c0365915403991a942b69c9c33dbb
TRETYAKweb <tretyakbro@gmail.com>
2023-01-10 22:38:59 +0300

    fix(pages): adding a component: Settings

--- src/pages/all-pages/index.js
+++ src/pages/all-pages/index.js
@@ -1,11 +1,16 @@
-import { Hero, Nav, TipList } from '../../components';
+import {
+    Hero, Nav, TipList, Settings,
+} from '../../components';
 
 export const AllTopicsPage = () => {
     return (
-        <section className = 'layout'>
-            <Nav />
-            <Hero />
-            <TipList />
-        </section>
+        <>
+            <Settings />
+            <section className = 'layout'>
+                <Nav />
+                <Hero />
+                <TipList />
+            </section>
+        </>
     );
 };
