10a4d604a492de157c0df7cde285ff2faa90b38f
TRETYAKweb <tretyakbro@gmail.com>
2023-01-14 00:16:26 +0300

    feat(hooks): implementation useTips

---
+++ src/hooks/useTips.js
@@ -0,0 +1,16 @@
+
+import { useQuery } from 'react-query';
+
+import { api } from '../api';
+
+export const useTips = () => {
+    const query = useQuery('tips', api.getTips);
+
+    const { data, isFetched } = query;
+
+    return {
+        data: Array.isArray(data) ? data : [],
+        isFetched,
+    };
+};
+
