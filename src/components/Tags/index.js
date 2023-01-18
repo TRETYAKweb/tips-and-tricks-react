d8e1f3f5e36e30e49ee246608dd5d03de845f8c2
TRETYAKweb <tretyakbro@gmail.com>
2023-01-18 11:00:18 +0300

    refactor(components): Tags

--- src/components/Tags/index.js
+++ src/components/Tags/index.js
@@ -8,19 +8,19 @@ import { useTags } from '../../hooks';
 import { getTagIcon, fetchify } from '../../helpers';
 import { Context } from '../../lib/selectedTagContext';
 
-export const Tags = () => {
+export const Tags = ({ tipViewMode }) => {
     const { data, isFetched } = useTags();
 
     const [selectedTagId, setSelectedTagId] = useContext(Context);
 
-    const tagsJSX = data.map(({ id, name }) => {
+    const tagsJSX = Array.isArray(data) && data.map(({ id, name }) => {
         const TagIcon = getTagIcon(name);
         const handleClick = () => setSelectedTagId(id);
 
         return (
             <span
                 onClick = { handleClick }
-                data-active = { selectedTagId === id } key = { id }
+                data-active = { selectedTagId === id || tipViewMode === 'all-topics' } key = { id }
                 className = 'tag'>
                 <TagIcon />
                 { name }
