9db7b7d8f11b97b9b4670834dd34f14a1817c14f
TRETYAKweb <tretyakbro@gmail.com>
2023-01-18 10:45:15 +0300

    feat(pages): add TopicByTagPage

---
+++ src/pages/topic-by-tag-page/index.js
@@ -0,0 +1,15 @@
+import {
+    Hero, Nav, TipList,
+} from '../../components';
+
+export const TopicByTagPage = () => {
+    return (
+        <>
+            <section className = 'layout'>
+                <Nav />
+                <Hero tipViewMode = 'topic-by-tag' />
+                <TipList tipViewMode = 'topic-by-tag' />
+            </section>
+        </>
+    );
+};
