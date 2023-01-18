9b440d99713a9ebf4b77df0ce647f209c5881658
TRETYAKweb <tretyakbro@gmail.com>
2023-01-18 10:59:46 +0300

    fix(components): TipList: add Context && tipViewMode

--- src/components/TipList/Composer/index.js
+++ src/components/TipList/Composer/index.js
@@ -1,5 +1,11 @@
+// Core
+import { useContext } from 'react';
+
 // Components
 import { Tip } from '../Tip';
+
+// Context
+import { Context } from '../../../lib/selectedTagContext';
 
 // Hooks
 import { useTips } from '../../../hooks';
@@ -7,10 +13,17 @@ import { useTips } from '../../../hooks';
 // Helpers
 import { fetchify } from '../../../helpers/fetchify';
 
-export const TipList = () => {
+export const TipList = ({ tipViewMode }) => {
     const { data, isFetched } = useTips();
+    const [selectedTagId] = useContext(Context);
 
-    const tipsJSX = data.map((tip) => <Tip key = { tip.id } { ...tip } />);
+    let tips = data;
+
+    if (tipViewMode === 'topic-by-tag' && Array.isArray(data)) {
+        tips = data.filter((tip) => tip.tag.id === selectedTagId);
+    }
+
+    const tipsJSX = tips.map((tip) => <Tip key = { tip.id } { ...tip } />);
 
     return (
         <section className = 'tip-list'>
