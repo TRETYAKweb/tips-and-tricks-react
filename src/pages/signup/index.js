b65a6d5bbaa5cf973f3715bc80df20d581f6dafe
TRETYAKweb <tretyakbro@gmail.com>
2023-01-21 01:43:14 +0300

    feat(pages): add SignUpPage

---
+++ src/pages/signup/index.js
@@ -0,0 +1,12 @@
+import { Nav } from '../../components';
+import { SignUpForm } from '../../components/forms/SignUpForm';
+
+export const SignUpPage = () => {
+    return (
+        <section className = 'layout'>
+            <Nav />
+
+            <SignUpForm />
+        </section>
+    );
+};
