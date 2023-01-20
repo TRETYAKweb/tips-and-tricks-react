a6a0fa072e31534a4915fdb5603e1f1372014b07
TRETYAKweb <tretyakbro@gmail.com>
2023-01-21 01:45:38 +0300

    chore(pages): re-export: PublishPage, SignUpPage, LoginPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,6 @@
 export { AllTopicsPage } from './all-pages';
 export { TopicByTagPage } from './topic-by-tag-page';
 export { TipByIdPage } from './tip-by-id';
+export { PublishPage } from './publish';
+export { SignUpPage } from './signup';
+export { LoginPage } from './login';
