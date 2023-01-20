8719e739925db7a305ecd30bbb7305fbf0e93b36
TRETYAKweb <tretyakbro@gmail.com>
2023-01-21 01:43:23 +0300

    feat(pages): add PublishPage

---
+++ src/pages/publish/index.js
@@ -0,0 +1,12 @@
+import { Nav } from '../../components';
+import { PublishTipForm } from '../../components/forms/PublishTipForm';
+
+export const PublishPage = () => {
+    return (
+        <section className = 'layout'>
+            <Nav />
+
+            <PublishTipForm />
+        </section>
+    );
+};
