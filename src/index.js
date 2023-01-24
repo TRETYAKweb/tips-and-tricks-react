5928e9ca2005d18309e96570d1e818334a6a67e8
TRETYAKweb <tretyakbro@gmail.com>
2023-01-24 08:56:10 +0300

    fix(core): connect: mobx-configure, StoreProvider, toast

--- src/index.js
+++ src/index.js
@@ -3,25 +3,34 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { configure } from 'mobx';
+import { toast, ToastContainer, Slide } from 'react-toastify';
 
 // Components
 import { App } from './App';
 
 // Instruments
 import './theme/main.scss';
+import 'react-toastify/dist/ReactToastify.css';
 
 // Context Provider
-import { SelectedTagProvider, SettingsProvider, queryClient } from './lib';
+import { queryClient, StoreProvider } from './lib';
+
+configure({
+    enforceActions:             'always',
+    computedRequiresReaction:   true,
+    observableRequiresReaction: true,
+    reactionRequiresObservable: true,
+});
 
 render(
     <QueryClientProvider client = { queryClient }>
-        <SettingsProvider>
-            <SelectedTagProvider>
-                <Router>
-                    <App />
-                </Router>
-            </SelectedTagProvider>
-        </SettingsProvider>
+        <ToastContainer newestOnTop transition = { Slide } />
+        <StoreProvider>
+            <Router>
+                <App />
+            </Router>
+        </StoreProvider>
         <ReactQueryDevtools initialIsOpen = { false } />
     </QueryClientProvider>,
     document.getElementById('root'),
