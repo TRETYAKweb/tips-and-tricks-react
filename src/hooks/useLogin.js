c645a820c0a7ef955dea18b76e6f75314882a9de
TRETYAKweb <tretyakbro@gmail.com>
2023-01-24 08:48:38 +0300

    fix(hooks): useLogin

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -5,11 +5,18 @@ import { useNavigate } from 'react-router-dom';
 
 // Instruments
 import { api } from '../api';
+import { useStore } from './useStore';
 
 export const useLogin = () => {
+    const { authStore } = useStore();
+    const { setError, setToken } = authStore;
     const navigate = useNavigate();
     const mutation = useMutation((credentials) => {
         return api.login(credentials);
+    }, {
+        onError(error) {
+            setError(error?.response?.data?.message);
+        },
     });
 
     useEffect(() => {
@@ -17,6 +24,7 @@ export const useLogin = () => {
 
         if (mutation.isSuccess && token) {
             localStorage.setItem('token', token);
+            setToken(token);
             navigate('/all-topics');
         }
     }, [mutation.isSuccess]);
