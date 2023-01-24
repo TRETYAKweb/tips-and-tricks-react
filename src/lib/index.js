9335e14f890acedb63a778018e5adce531ca2534
TRETYAKweb <tretyakbro@gmail.com>
2023-01-24 08:45:38 +0300

    chore(lib): re-export: StoreProvider

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,2 @@
-export { SelectedTagProvider } from './selectedTagContext';
-export { SettingsProvider } from './settingsContext';
 export { queryClient } from './queryClient';
+export { StoreProvider } from './storeContext';
