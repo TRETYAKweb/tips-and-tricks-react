ed40ab4a95c8c7d58413ee57dccb840046afe9cd
TRETYAKweb <tretyakbro@gmail.com>
2023-02-15 09:09:39 +0300

    fix(core): cleaning mobx

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
 // Core
-import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
 import {
     Navigate, Outlet, Route, Routes,
 } from 'react-router-dom';
-import { toast } from 'react-toastify';
 // Components
 import { Settings } from './components';
-import { useStore } from './hooks';
+import { useErrorMessage } from './hooks/useErrorMessage';
 import {
     AllTopicsPage, TopicByTagPage, TipByIdPage, PublishPage, LoginPage, SignUpPage,
 } from './pages';
 
 
-export const App = observer(() => {
-    const { authStore } = useStore();
-    const { errorMessage, resetError } = authStore;
-
-    useEffect(() => {
-        if (errorMessage) {
-            const notify = () => toast.error(errorMessage, {
-                position:        'top-right',
-                autoClose:       5000,
-                hideProgressBar: false,
-                closeOnClick:    true,
-                pauseOnHover:    true,
-                draggable:       true,
-                progress:        undefined,
-            });
-
-            notify();
-
-            // необходимо очистить состояние ошибки чтобы
-            //  при повторном возникновении такой же ошибки появилось
-            //  всплывающиее сообщение
-
-            resetError();
-        }
-    }, [errorMessage]);
+export const App = () => {
+    useErrorMessage();
 
     return (
         <>
@@ -68,4 +42,4 @@ export const App = observer(() => {
             </Routes>
         </>
     );
-});
+};
