856893e7aad32c9b98449526977505a1be6e616b
TRETYAKweb <tretyakbro@gmail.com>
2023-04-16 18:39:59 +0300

    feat(redux): convert tags reducer to TypeScript
    Converted the tags reducer from JavaScript to TypeScript by adding type annotations for the state and the action parameter.

--- src/lib/redux/reducers/tags.ts
+++ src/lib/redux/reducers/tags.ts
@@ -1,10 +1,12 @@
+import { AnyAction } from 'redux';
+
 import { tagTypes } from '../types';
 
 const initialState = {
     selectedTagId: null,
 };
 
-export const tagsReducer = (state = initialState, action) => {
+export const tagsReducer = (state = initialState, action:AnyAction) => {
     switch (action.type) {
         case tagTypes.SET_SELECTED_TEG_ID: {
             return { ...state, selectedTagId: action.payload };
