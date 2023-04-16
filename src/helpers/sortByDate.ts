f29bdcf6dc88c7ce03e42aa681194521ce1ee576
TRETYAKweb <tretyakbro@gmail.com>
2023-04-16 08:21:43 +0300

    feat: convert sortByDate helper function to TypeScript
    The sortByDate helper function in src/helpers/sortByDate.js has been converted to TypeScript in order to provide stronger typing and improve code clarity.
    
    Changes Made:
    - Imported the ITipModel type from the types file.
    - Updated the function signature to include the ITipModel type.
    - Used the new signature throughout the codebase.
    
    This addresses the issue of weak typing in sortByDate and provides better code maintainability.

--- src/helpers/sortByDate.ts
+++ src/helpers/sortByDate.ts
@@ -1,4 +1,6 @@
+import { ITipModel } from '../types';
+
 // Хелпер функция для сортировки дат.
-export const sortByDate = (a, b) => {
+export const sortByDate = (a: ITipModel, b: ITipModel) => {
     return new Date(b.created).getMilliseconds() - new Date(a.created).getMilliseconds();
 };
