2b771a208a9a25536e00c1c565402ec23de7d48e
TRETYAKweb <tretyakbro@gmail.com>
2023-04-16 08:24:19 +0300

    feat: convert useCreateTip hook to TypeScript
    The useCreateTip hook has been converted to TypeScript, with the `tip` parameter now typed as an `IPublishFormShape` object. This change ensures type safety throughout the hook and its associated components.

--- src/hooks/useCreateTip.ts
+++ src/hooks/useCreateTip.ts
@@ -5,15 +5,16 @@ import { useSelector } from 'react-redux';
 // Instruments
 import { api } from '../api';
 import { getToken } from '../lib/redux/selectors';
+import { IPublishFormShape } from '../components/forms/Types';
 
 export const useCreateTip = () => {
     const client = useQueryClient();
     const token = useSelector(getToken);
-    const mutation = useMutation((tip) => {
+    const mutation = useMutation((tip:IPublishFormShape) => {
         return api.createTip(tip, token);
     }, {
         onSettled() {
-            client.invalidateQueries('./tips');
+            void client.invalidateQueries('./tips');
         },
     });
 
