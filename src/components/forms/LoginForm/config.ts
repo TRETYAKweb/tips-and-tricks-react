27dea06c9c83253000dd4bfd6266cb30c87eb311
TRETYAKweb <tretyakbro@gmail.com>
2023-04-16 07:34:23 +0300

    refactor(LoginForm): convert config.js to TypeScript
    The config.js file used in the LoginForm component has been converted to TypeScript by renaming it to config.ts and adding a type for the shape of the login form. The yup schema has also been updated to use this type.

--- src/components/forms/LoginForm/config.ts
+++ src/components/forms/LoginForm/config.ts
@@ -1,12 +1,13 @@
 // Core
 import * as yup from 'yup';
+import { ILoginFormShape } from '../Types';
 
 // eslint-disable-next-line no-template-curly-in-string
 const tooShortMessage = 'Минимальная длина - ${min} символов';
 // eslint-disable-next-line no-template-curly-in-string
 const tooLongMessage = 'максимальная длина - ${max} символов';
 
-export const schema = yup.object().shape({
+export const schema: yup.SchemaOf<ILoginFormShape> = yup.object().shape({
     email:    yup.string().email().required('*'),
     password: yup.string().min(8, tooShortMessage).max(16, tooLongMessage).required('*'),
 });
