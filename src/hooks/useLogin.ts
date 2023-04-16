36638c1d8684a01719b06747be684354336816f7
TRETYAKweb <tretyakbro@gmail.com>
2023-04-16 08:29:33 +0300

    feat: add useLogin hook
    Add the useLogin hook to handle login functionality. The hook uses useMutation from react-query to handle API calls, and useDispatch from react-redux to update the user's state in the Redux store. The hook also uses the useNavigate hook from react-router-dom to redirect the user after logging in. The onError callback is used to handle any errors that occur during the login process, such as a 401 Unauthorized error, and displays an error message using the toast library.

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -5,16 +5,18 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 // Instruments
+import { AxiosError } from 'axios';
 import { api } from '../api';
 import { authActions } from '../lib/redux/actions';
+import { ILoginFormShape } from '../components/forms/Types';
 
 export const useLogin = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const mutation = useMutation((credentials) => {
+    const mutation = useMutation((credentials: ILoginFormShape) => {
         return api.login(credentials);
     }, {
-        onError(error) {
+        onError(error: AxiosError) {
             const { response } = error;
 
             if (response?.status === 401) {
