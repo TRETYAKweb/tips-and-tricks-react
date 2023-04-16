941c820a30fff5a7402bb7f57230650ecad247b7
TRETYAKweb <tretyakbro@gmail.com>
2023-04-16 18:39:29 +0300

    feat: add settings reducer in TS
    Converted src/lib/redux/reducers/settings.js to TypeScript.

--- src/lib/redux/reducers/settings.ts
+++ src/lib/redux/reducers/settings.ts
@@ -1,10 +1,12 @@
+import { AnyAction } from 'redux';
+
 import { settingsTypes } from '../types';
 
 const initialState = {
     isSettingsOpen: false,
 };
 
-export const settingsReducer =  (state = initialState, action) => {
+export const settingsReducer =  (state = initialState, action: AnyAction) => {
     switch (action.type) {
         case settingsTypes.SET_SETTINGS_OPEN: {
             return {
