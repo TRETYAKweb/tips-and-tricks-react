1d06e7709c818c2e2d38453773777933a1adb2c7
TretyakWeb <tretyakbro@gmail.com>
2023-06-15 22:10:47 +0300

    fix(sortByDate): fix date sorting by using getTime() method

--- src/helpers/sortByDate.ts
+++ src/helpers/sortByDate.ts
@@ -2,5 +2,5 @@ import { ITipModel } from '../types';
 
 // Хелпер функция для сортировки дат.
 export const sortByDate = (a: ITipModel, b: ITipModel) => {
-    return new Date(b.created).getMilliseconds() - new Date(a.created).getMilliseconds();
+    return new Date(b.created).getTime() - new Date(a.created).getTime();
 };
